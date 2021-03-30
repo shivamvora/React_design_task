656ea36a6b34ef62181382170ca8011d06909612
shivamvora <shivampatel27102001@gmail.com>
2021-03-30 12:23:19 +0530

    design done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1 className="text">CALCULATOR</h1>
+      </div>
+      <div className="cal__body">
+        <div className="cal__content">
+          <div className="format__cal">
+            <p className="inside__font">Enter the numbers</p>
+            <div className="input_box">
+              <br /><br />
+              <input type="text" className="style_input" placeholder="number 1" />
+              <br />
+              <input type="text" className="style_input" placeholder="number 2" />
+              <br />
+              <button className="style_button">Sum</button>
+
+              <div>
+                <hr className="hr__style" />
+              </div>
+              <div className="result__show">
+                <p className="inside__font">Results</p>
+              </div>
+              <div>
+                <input type="text" placeholder="1523" className="style_input_result" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
